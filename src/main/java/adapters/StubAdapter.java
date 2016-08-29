649517ed90884122afb2305ea0a09f977f1413da
Edwin Herrera <edwherrera.175@gmail.com>
2016-08-28 21:13:31 -0600

    generate random intermediate stop

--- src/main/java/adapters/StubAdapter.java
+++ src/main/java/adapters/StubAdapter.java
@@ -6,19 +6,18 @@ import models.FareSearchRequest;
 import models.Flight;
 import models.FlightGroup;
 import models.Itinerary;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class StubAdapter {
 
     private final TerminalRepository terminalRepository;
-    private final AirlineRepository airlineRepository;
 
     public StubAdapter() {
         terminalRepository = new TerminalRepository();
-        airlineRepository = new AirlineRepository();
     }
 
     public Itinerary fareSearch(final FareSearchRequest request) {
@@ -27,19 +26,88 @@ public class StubAdapter {
         itinerary.setTotalFareAmount(fareAmount);
         itinerary.setTotalTaxAmount(fareAmount * 7.5F);
 
-        FlightGroup inComing = new FlightGroup();
+        List<FlightGroup> flightGroups = new ArrayList<>();
 
-        for (int i = 0; i < 10; i += 1) {
-            final FlightGroup outGoing = new FlightGroup();
+        for (int i = 0; i < 250; i += 1) {
+            final FlightGroup flightGroup = new FlightGroup();
 
-            Flight outFlight = new Flight();
-            outFlight.setDepartureDate(request.getDepartureDate().toString());
-            outFlight.setDepartureGate(request.getDepartureGateway());
-            outFlight.setDepartureTerminal(
+            Flight flight = new Flight();
+            flight.setDepartureDate(request.getDepartureDate().toString());
+            flight.setDepartureGate(request.getDepartureGateway());
+            flight.setDepartureTerminal(
                     terminalRepository.allTerminals().get(generateRandom(0, terminalRepository.allTerminals().size())));
+            flight.setDepartureTime(randomTime());
+            flight.setArrivalDate(request.getDepartureDate().toString());
+            flight.setAircraftType(randomAircraft());
+            flight.setArrivalTerminal(
+                    terminalRepository.allTerminals().get(generateRandom(0, terminalRepository.allTerminals().size())));
+            flight.setArrivalTime(randomlyAddedTime());
+            flight.setCabinType("???????");
+            flight.setFlightNumber("" + generateRandom(1, 5000));
+            flight.setRedEye(generateRandom(0, 2) == 1);
+            flight.setMarketingCarrier("???????");
+            flight.setOperatingCarrier("???????");
+            flight.setArrivalGateway(randomGateway());
+
+            if (request.getNonstop() || (generateRandom(0, 500) > 250)) {
+                flightGroup.setFlights(Collections.singletonList(flight));
+            } else {
+                Flight intermediateFlight = new Flight();
+                intermediateFlight.setDepartureDate(request.getDepartureDate().toString());
+                intermediateFlight.setDepartureGate(flight.getArrivalGateway());
+                intermediateFlight.setDepartureTerminal(
+                        terminalRepository.allTerminals().get(generateRandom(0, terminalRepository.allTerminals().size())));
+                intermediateFlight.setDepartureTime(randomlyAddedTime());
+                intermediateFlight.setAircraftType(randomAircraft());
+                intermediateFlight.setArrivalTime(randomlyAddedTime());
+                intermediateFlight.setCabinType("??????");
+                intermediateFlight.setFlightNumber("" + generateRandom(1, 5000));
+                intermediateFlight.setRedEye(generateRandom(0, 2) == 1);
+                intermediateFlight.setMarketingCarrier("?????");
+                intermediateFlight.setOperatingCarrier("???????");
+                intermediateFlight.setArrivalGateway(randomGateway());
+
+                flightGroup.setFlights(Arrays.asList(flight, intermediateFlight));
+            }
+
+            flightGroup.setFlightTime(calculateFlightTime());
+            flightGroup.setTravelTime(calculateTravelTime());
+            flightGroup.setMixedCabins(generateRandom(0,2) == 1);
+
+            flightGroups.add(flightGroup);
         }
 
-        throw new NotImplementedException();
+
+        itinerary.setFlightGroups(flightGroups);
+        return itinerary;
+    }
+
+    private String calculateTravelTime() {
+        return "";
+    }
+
+    private String calculateFlightTime() {
+        return "";
+    }
+
+    private String randomGateway() {
+        return "";
+    }
+
+    private String randomCabinType() {
+        return "";
+    }
+
+    private String randomlyAddedTime() {
+        return "";
+    }
+
+    private String randomAircraft() {
+        return "";
+    }
+
+    private String randomTime() {
+        return "";
     }
 
     private int generateRandom(int min, int max) {
